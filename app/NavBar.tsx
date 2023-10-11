38a1f2b78560d1f40646c28f1295205a5a6e8a92
Tony Childs <tony.childs@gmail.com>
2023-10-11 10:33:54 -0500

    Animate/rotate bug icon on hover

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,7 +14,10 @@ const NavBar = () => {
   return (
     <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-5">
       <Link href="/">
-        <GiSpottedBug size={30} className="fill-red-600 hover:scale-125" />
+        <GiSpottedBug
+          size={30}
+          className="fill-red-600 transition-transform hover:rotate-45"
+        />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
