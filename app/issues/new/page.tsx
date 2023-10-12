997f8e121e020fa133b61977f38850951cf1611f
Tony Childs <tony.childs@gmail.com>
2023-10-12 17:19:23 -0500

    Extract ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { FaExclamationTriangle } from "react-icons/fa";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { createIssueSchema } from "@/app/validationSchemas";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type CreateIssueForm = z.infer<typeof createIssueSchema>;
 
@@ -52,11 +53,7 @@ const NewIssuePage = () => {
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      {errors.title && (
-        <Text color="red" as="p">
-          {errors.title.message}
-        </Text>
-      )}
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
       <Controller
         name="description"
         control={control}
@@ -64,11 +61,7 @@ const NewIssuePage = () => {
           return <SimpleMDE placeholder="Description" {...field} />;
         }}
       />
-      {errors.description && (
-        <Text color="red" as="p">
-          {errors.description.message}
-        </Text>
-      )}
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
       <Button>Submit New Issue</Button>
     </form>
   );
