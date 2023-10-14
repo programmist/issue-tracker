67cef851afc633c72a1aad6ff95a2f823e177c11
Tony Childs <tony.childs@gmail.com>
2023-10-14 16:07:53 -0500

    Add markdown rendering in issue detail

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -23,8 +24,10 @@ const IssueDetailPage = async ({ params }: Props) => {
         <StatusBadge status={issue.status} />
         <Text>{issue.createdAt.toLocaleDateString()}</Text>
       </Flex>
-      <Card>
-        <p>{issue.description}</p>
+      <Card className="mt-4">
+        <ReactMarkdown className="prose text-black marker:text-black">
+          {issue.description}
+        </ReactMarkdown>
       </Card>
     </div>
   );
