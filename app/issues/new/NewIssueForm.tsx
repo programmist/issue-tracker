f2585985ea8b36a82013d225247ad34618512bcc
Tony Childs <tony.childs@gmail.com>
2023-10-15 16:35:08 -0500

    Fix problems with new issue loading skeletons not applying uniformly

---
+++ app/issues/new/NewIssueForm.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { FaExclamationTriangle } from "react-icons/fa";
+import SimpleMDE from "react-simplemde-editor";
+import { z } from "zod";
+
+type CreateIssueForm = z.infer<typeof createIssueSchema>;
+
+const NewIssueForm = () => {
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateIssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("An unexpected error occurred");
+    } finally {
+      setSubmitting(false);
+    }
+  });
+
+  return (
+    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Icon>
+            <FaExclamationTriangle size={18} />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
+      <TextField.Root>
+        <TextField.Input placeholder="Title" {...register("title")} />
+      </TextField.Root>
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
+      {/* TODO Tweak blockquote > p bottom margins to add a quote author line.
+          Maybe can modify how the the quote is rendered to include an author
+          inline that has different styles (e.g. no ::before::after quotes, etc)
+      */}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => {
+          return (
+            <SimpleMDE
+              className="prose text-black marker:text-black"
+              placeholder="Description"
+              {...field}
+            />
+          );
+        }}
+      />
+      <ErrorMessage>{errors.description?.message}</ErrorMessage>
+      <Button disabled={submitting}>
+        Submit New Issue {submitting && <Spinner />}
+      </Button>
+    </form>
+  );
+};
+
+export default NewIssueForm;
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,85 +1,25 @@
-/* eslint-disable @next/next/no-async-client-component */
 "use client";
 
-import { Button, Callout, TextField, Text } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
 import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { FaExclamationTriangle } from "react-icons/fa";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { createIssueSchema } from "@/app/validationSchemas";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import dynamic from "next/dynamic";
+import NewIssueLoadingPage from "./loading";
+/**
+ * Empty wrapper component does two things.
+ * 1. Dynamically loads <NewIssueLoadingPage> component because it
+ *    mounts <SimpleMDE>m which is not SSR-compatible.
+ * 2. Loads the entire <form> dynamically so that loading skeletons can
+ *    be applied uniformly. Trying to dynamically load just SimpleMDE
+ *    and relying on default loading.tsx behavior caused loading skeletons
+ *    only to apply to the SimpleMDE component and not the Title input,
+ *    which looked bad.
+ */
+const NewIssueForm = dynamic(async () => import("./NewIssueForm"), {
+  ssr: false,
+  loading: () => <NewIssueLoadingPage />,
+});
 
-type CreateIssueForm = z.infer<typeof createIssueSchema>;
-
-const NewIssuePage = async () => {
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<CreateIssueForm>({
-    resolver: zodResolver(createIssueSchema),
-  });
-  const router = useRouter();
-  const [error, setError] = useState("");
-  const [submitting, setSubmitting] = useState(false);
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      setSubmitting(true);
-      await axios.post("/api/issues", data);
-      router.push("/issues");
-    } catch (error) {
-      setError("An unexpected error occurred");
-    } finally {
-      setSubmitting(false);
-    }
-  });
-
-  return (
-    <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
-      {error && (
-        <Callout.Root color="red">
-          <Callout.Icon>
-            <FaExclamationTriangle size={18} />
-          </Callout.Icon>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      <ErrorMessage>{errors.title?.message}</ErrorMessage>
-      {/* TODO Tweak blockquote > p bottom margins to add a quote author line.
-          Maybe can modify how the the quote is rendered to include an author
-          inline that has different styles (e.g. no ::before::after quotes, etc)
-      */}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => {
-          return (
-            <SimpleMDE
-              className="prose text-black marker:text-black"
-              placeholder="Description"
-              {...field}
-            />
-          );
-        }}
-      />
-      <ErrorMessage>{errors.description?.message}</ErrorMessage>
-      <Button disabled={submitting}>
-        Submit New Issue {submitting && <Spinner />}
-      </Button>
-    </form>
-  );
+const NewIssuePage = () => {
+  return <NewIssueForm />;
 };
 
 export default NewIssuePage;
