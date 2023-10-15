b15bd86a5f70ae7e85bb7c61b11822c53d6beff2
Tony Childs <tony.childs@gmail.com>
2023-10-15 17:19:13 -0500

    Add issue edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
-
 interface Props {
   params: { id: string };
 }
@@ -18,16 +19,24 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="5" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toLocaleDateString()}</Text>
-      </Flex>
-      <Card className="prose text-black marker:text-black" mt="4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="5" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toLocaleDateString()}</Text>
+        </Flex>
+        <Card className="prose text-black marker:text-black" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
