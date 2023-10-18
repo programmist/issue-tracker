3680bb1aa69627ecba7a4850c579e8f424260dcf
Tony Childs <tony.childs@gmail.com>
2023-10-18 00:25:34 -0500

    Add TODO regarding Next.js loading.tsx bug

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,12 @@ const IssueForm = dynamic(
   },
 );
 
+// TODO: There is a flash of the loading skeleton from the parent route (/issues)
+//       before this loading skeleton is shown. I believe this is related to the
+//       Github issue below. Mosh reorganizes his code to work-around this, but I
+//       don't think it's worth the disorganization it causes.
+//       Also, same thing happens with edit issue page.
+//       https://github.com/vercel/next.js/issues/56344
 const NewIssuePage = () => {
   return <IssueForm />;
 };
