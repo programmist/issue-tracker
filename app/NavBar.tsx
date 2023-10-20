2369056a19e29dd848baf00c443f8445371d4aee
Tony Childs <tony.childs@gmail.com>
2023-10-19 23:54:21 -0500

    Use radix-ui-flex-based layout strategy for navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { GiSpottedBug } from "react-icons/gi";
 import { useSession } from "next-auth/react";
-import { Box } from "@radix-ui/themes";
+import { Box, Flex, Container } from "@radix-ui/themes";
 import { Spinner } from "./components";
 
 const NavBar = () => {
@@ -17,37 +17,46 @@ const NavBar = () => {
   ];
 
   return (
-    <nav className="mb-5 flex h-14 items-center gap-6 border-b px-5">
-      <Link href="/">
-        <GiSpottedBug
-          size={30}
-          className="transition-fill duration-300 hover:rotate-45 hover:fill-red-600 "
-        />
-      </Link>
-      <ul className="flex space-x-6">
-        {links.map((link) => (
-          <li key={link.href}>
-            <Link
-              href={link.href}
-              className={classnames("transition-colors hover:text-zinc-800", {
-                "text-zinc-900": path === link.href,
-                "text-zinc-500": path !== link.href,
-              })}
-            >
-              {link.label}
+    <nav className="mb-5 border-b px-5 py-3">
+      <Container size="3">
+        <Flex justify="between" align="center">
+          <Flex align="center" gap="3">
+            <Link href="/">
+              <GiSpottedBug
+                size={30}
+                className="transition-fill duration-300 hover:rotate-45 hover:fill-red-600 "
+              />
             </Link>
-          </li>
-        ))}
-      </ul>
-      <Box className="ml-auto">
-        {status === "loading" && <Spinner />}
-        {status === "authenticated" && (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-        {status === "unauthenticated" && (
-          <Link href="/api/auth/signin">Log in</Link>
-        )}
-      </Box>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={classnames(
+                      "transition-colors hover:text-zinc-800",
+                      {
+                        "text-zinc-900": path === link.href,
+                        "text-zinc-500": path !== link.href,
+                      },
+                    )}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "loading" && <Spinner />}
+            {status === "authenticated" && (
+              <Link href="/api/auth/signout">Logout</Link>
+            )}
+            {status === "unauthenticated" && (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
